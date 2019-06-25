a25d5a4ef62fd5cda1451f1c85c678bd5703f362
cmming <2942257072@qq.com>
2019-06-26 00:45:17 +0800

    router

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
+'use strict'
+const path = require('path')
+// const defaultSettings = require('./src/settings.js')
+// const name = defaultSettings.title || 'vue Element Admin' // page title
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
-    // 选项...
-    publicPath: process.env.NODE_ENV === 'production'
-    ? 'http://www.baidu.com'
-    : '/'
-  }
+  // 选项...
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+  // configureWebpack: {
+  //   // provide the app's title in webpack's name field, so that
+  //   // it can be accessed in index.html to inject the correct title.
+  //   // name: name,
+  //   resolve: {
+  //     alias: {
+  //       '@': resolve('src')
+  //     }
+  //   }
+  // },
+}
