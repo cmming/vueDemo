4d804d61a11cc0d62530f53aed56ca94de1e6381
chmi <294225707@qq.com>
2019-08-14 14:47:18 +0800

    1.测试 travis

--- src/moudles/user/views/list.vue
+++ src/moudles/user/views/list.vue
@@ -1,27 +1,65 @@
 <template>
   <div>
     <el-card class="m-bottom-sm search-area">
-      <search-area :model="user" @addItem="addItem">
-         <template slot="btn">
-          <el-button @click="exportExcel" type="success">导出excel</el-button>
+      <search-area
+        :model="user"
+        @addItem="addItem"
+      >
+        <template slot="btn">
+          <el-button
+            @click="exportExcel"
+            type="success"
+          >导出excel</el-button>
         </template>
       </search-area>
     </el-card>
     <el-card>
-      <c-table :model="user" @editItem="editItem">
-        <template slot="customAction" slot-scope="scope">
+      <c-table
+        :model="user"
+        @editItem="editItem"
+      >
+        <template
+          slot="customAction"
+          slot-scope="scope"
+        >
           <el-button @click="setRoles(scope.dataScope.row)">角色管理</el-button>
         </template>
       </c-table>
     </el-card>
+    <!-- <vitual-form :action="action" ref="exportExcel"></vitual-form> -->
+    <form
+      :action="action"
+      method="get"
+      style="display:none"
+      target="hideIframe"
+    >
+      <input
+        type="submit"
+        ref="downloadBtn"
+      >
+    </form>
+    <iframe
+      name="hideIframe"
+      src="about:blank"
+      style="display:none;"
+    ></iframe>
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
 import requestMap from "@/api/requestMap";
+import VitualForm from "./vitualForm";
 export default {
   computed: {
     ...mapGetters(["user"])
+  },
+  components: {
+    VitualForm
+  },
+  data() {
+    return {
+      action: process.env.VUE_APP_BASE_API + "/user/export"
+    };
   },
   created() {
     // eslint-disable-next-line
@@ -37,14 +75,25 @@ export default {
         type: "success"
       });
     },
-    editItem(data){
-      this.$router.push('/admin/user/update/'+data.id)
+    editItem(data) {
+      this.$router.push("/admin/user/update/" + data.id);
     },
     addItem() {
       this.$router.push("/admin/user/store");
     },
-    exportExcel(){
-      requestMap('EXPORT_USER')
+    exportExcel() {
+      requestMap('EXPORT_USER').then(res=>{
+        console.log(res)
+        //  var a = document.createElement("a");
+        // a.href = res.data;
+        // a.download = 11;
+        // document.body.appendChild(a);
+        // a.click();
+        // a.remove();
+      })
+      // this.jumpDownload();
+      // console.log(this.$refs["downloadBtn"]);
+      // this.$refs["downloadBtn"].click();
     }
   }
 };
