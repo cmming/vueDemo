2d8334015393814b65d375536af2d173c06068fc
chmi <294225707@qq.com>
2019-08-17 15:03:55 +0800

    为下载文件添加响应头

--- src/moudles/user/views/list.vue
+++ src/moudles/user/views/list.vue
@@ -48,6 +48,7 @@
 <script>
 import { mapGetters } from "vuex";
 import requestMap from "@/api/requestMap";
+import request from "@/api/request";
 import VitualForm from "./vitualForm";
 export default {
   computed: {
@@ -82,15 +83,37 @@ export default {
       this.$router.push("/admin/user/store");
     },
     exportExcel() {
-      requestMap('EXPORT_USER').then(res=>{
-        console.log(res)
-        //  var a = document.createElement("a");
-        // a.href = res.data;
-        // a.download = 11;
-        // document.body.appendChild(a);
-        // a.click();
-        // a.remove();
-      })
+      request({
+        url: "/user/export",
+        method: "get",
+        hasData: false,
+        responseType: "arraybuffer"
+      }).then(res => {
+        if (res.status === 200 && res.data) {
+          var disposition = res.headers["content-disposition"];
+          var fileName = decodeURI(
+            disposition.substring(
+              disposition.indexOf("filename=") + 9,
+              disposition.length
+            )
+          );
+          let blob = new Blob([res.data], { type: res.headers["content-type"] }); // 假设文件为pdf
+          let link = document.createElement("a");
+          link.href = window.URL.createObjectURL(blob);
+          link.download = fileName;
+          link.click();
+          link.remove();
+        }
+      });
+      // requestMap("EXPORT_USER").then(res => {
+      //   console.log(res);
+      //   //  var a = document.createElement("a");
+      //   // a.href = res.data;
+      //   // a.download = 11;
+      //   // document.body.appendChild(a);
+      //   // a.click();
+      //   // a.remove();
+      // });
       // this.jumpDownload();
       // console.log(this.$refs["downloadBtn"]);
       // this.$refs["downloadBtn"].click();
