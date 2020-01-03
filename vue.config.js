ff2bb5f65c93aeda6055e72556ca8c9c03ade9a9
chmi <294225707@qq.com>
2020-01-03 14:23:51 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: true,
+  productionSourceMap: false,
 
   devServer: {
     proxy: {
